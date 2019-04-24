5a549046846eba7246d48378d8a7d912927eadce
Chase Zhou <czhou@mitre.org>
2019-04-24 13:36:25 -0400

    make partial classes instead of sending everything

--- lib/app.rb
+++ lib/app.rb
@@ -1,24 +1,13 @@
 require 'inferno-smart-launch'
 require 'dm-core'
 require_relative './sequence_extension'
-
-def self.property(name, type, default=nil)
-	if default.nil? then
-		Inferno::Models::TestingInstance.send("property", name, type) 
-	else
-		Inferno::Models::TestingInstance.send("property", name, type, default) 
-	end
-end
-
-property :dynamically_registered, DataMapper::Property::Boolean
+require_relative './testing_instance'
 
 
 DataMapper.auto_upgrade!
-
-
-Inferno::Sequence::SequenceBase.send("include", SequenceExtension)
 
 Inferno::Sequence.load_sequences(__dir__)
 Inferno::Module.load_modules(__dir__)
 
 Inferno::App::Endpoint::Landing.send("set", :modules, ['onc'])
+Rack::Handler::Thin.run Inferno::App.new
